f6bef94f0ee8428823feb74e310b60949b8493ea
Shreyash Damodar <shreyashdamodar1086@gmail.com>
2025-04-26 23:12:06 +0530

    added the middleware logic

---
+++ BasicCourseApp/middlewares/admin.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken")
+const {ADMIN_SECRET} = require("../config")
+
+function adminMiddleware(req, res, next) {
+    const token = req.header.token;
+    const decodeToken = jwt.verify(token, ADMIN_SECRET)
+    if (decodeToken) {
+        req.userId = decodeToken.id
+        next()
+    } else {
+        res.status(403).json({
+            messege : "you are not signed in"
+        })
+    }
+}
+
+module.exports = {
+    adminMiddleware : adminMiddleware
+}
---
+++ BasicCourseApp/middlewares/user.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken")
+const {USERS_SECRET} = require("../config")
+
+function userMiddleware(req, res, next) {
+    const token = req.header.token;
+    const decodeToken = jwt.verify(token, USERS_SECRET)
+    if (decodeToken) {
+        req.userId = decodeToken.id
+        next()
+    } else {
+        res.status(403).json({
+            messege : "you are not signed in"
+        })
+    }
+}
+
+module.exports = {
+    userMiddleware : userMiddleware
+}
