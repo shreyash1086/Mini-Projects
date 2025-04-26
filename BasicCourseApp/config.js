e4ec7967244dbb8cc05d433dfabfe99b90fd9a3a
Shreyash Damodar <shreyashdamodar1086@gmail.com>
2025-04-26 17:54:12 +0530

    adding a config.js file for the sensetive information

---
+++ BasicCourseApp/config.js
@@ -0,0 +1,9 @@
+require("dotenv").config()
+
+const USERS_SECRET = process.env.USERS_SECRET
+const ADMIN_SECRET = process.env.ADMIN_SECRET
+
+module.exports = {
+    USERS_SECRET: USERS_SECRET,
+    ADMIN_SECRET: ADMIN_SECRET
+}
